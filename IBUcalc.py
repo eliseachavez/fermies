a1f11b24c02e5f013fc23e78a190b48079b890c6
MycoAu <47406095+MycoAu@users.noreply.github.com>
2019-02-07 20:48:58 -0600

    add spec & IBUcalc, remove test

---
+++ IBUcalc.py
@@ -0,0 +1,31 @@
+# This is the start of the IBU calculator
+
+#   DEFINITIONS
+
+def AAUcalc(weight,percAA):
+    #weight is in oz as a float, alpha acids are a percent to the nearest 1/10th
+    weight=float(input())
+    percAA=float(input())
+    
+
+
+def IBUcalc(variable1,variable2,vari...):
+    hops="centenllia"
+    aa=5.2
+    boiltime=60
+    vol=5
+    utilization=.1
+
+    return ____
+
+
+
+
+
+# MAIN
+stuff
+stuff
+stuff
+
+ibuproduct= IBUcalc(hops,aa,boiltime,vol,utilization)
+
